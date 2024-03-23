d968e0ceb23928070c1d26cac027616f5a6abd77
Jzakka <mouse4786@gmail.com>
2024-03-23 22:23:06 +0900

    Initial commit

---
+++ src/main/java/com/march/bad_comments_filter/BadCommentsFilterApplication.java
@@ -0,0 +1,13 @@
+package com.march.bad_comments_filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadCommentsFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadCommentsFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/march/bad_comments_filter/BadCommentsFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.march.bad_comments_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadCommentsFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
